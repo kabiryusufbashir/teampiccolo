2a7858a033d43810b3b991031ec907042b55437e
piccolo <kabiryusufbashir@gmail.com>
2019-09-17 19:39:48 -0700

    Created the logo and navigation section inside the header tag

--- index.php
+++ index.php
@@ -7,7 +7,24 @@
     <link rel="stylesheet" href="./css/master.css">
   </head>
   <body>
-    <header></header>
+    <header>
+      <div class="logo">
+        <img src="./images/logo.png" alt="Team Piccolo Global Enterprises Logo">
+      </div>
+      <div class="nav-section">
+        <nav>
+          <ul>
+            <li>Home</li>
+            <li>About Us</li>
+            <li>Services</li>
+            <li>Portfolio</li>
+            <li>Our Team</li>
+            <li>Web Development Class</li>
+            <li>Contact Us</li>
+          </ul>
+        </nav>
+      </div>
+    </header>
     <main></main>
     <footer></footer>
   </body>
