203a020b37ea2e2c29ab0a60e83b1b924c1c1ce2
piccolo <kabiryusufbashir@gmail.com>
2019-09-17 19:32:32 -0700

    Added the page ICON and title

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Team Piccolo Global Enterprises</title>
+    <link rel="icon" href="./images/favicon.ico">
+  </head>
+  <body>
+
+  </body>
+</html>
