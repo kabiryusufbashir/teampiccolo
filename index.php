fb88b2789d98488a0f185b8e6f2a44cce09e8056
piccolo <kabiryusufbashir@gmail.com>
2019-09-18 22:10:54 -0700

    Styled the about us section of the page

--- index.php
+++ index.php
@@ -84,13 +84,25 @@
         </div>
       </section>
       <section id="about-team-piccolo">
-        <div class="col-fill">
-          <img src="./images/ceo.jpg" alt="Kabir Yusuf Bashir CEO Team Piccolo">
+        <div class="col-full">
+          <div class="col-fill">
+            <img src="./images/ceo.jpg" alt="Kabir Yusuf Bashir CEO Team Piccolo">
+            <p>Kabir Yusuf Bashir CEO Team Piccolo Global Enterprises</p>
+          </div>
+          <div class="col-extra">
+            <p>Team Piccolo Global Enterprises is registered with Corporate Affairs Commission (C.A.C) under COMPANIES AND ALLIED MATTERS ACT 1990 on the 25th day of November, 2016 with a Business Number (B.N) 2462226</p>
+            <p>Over the years, Team Piccolo Global Enterprises has grown to a major ICT solution and Academic Writing provider in Nigeria and Africa, with major projects realized in different parts of the country and region</p>
+          </div>
         </div>
-        <div class="col-extra">
-          <p>Team Piccolo Global Enterprises is registered with Corporate Affairs Commission (C.A.C) under COMPANIES AND ALLIED MATTERS ACT 1990 on the 25th day of November, 2016 with a Business Number (B.N) 2462226</p>
-          <p>Over the years, Team Piccolo Global Enterprises has grown to a major ICT solution and Academic Writing provider in Nigeria and Africa, with major projects realized in different parts of the country and region</p>
-          <p>As a wholly indigenous and dynamic ICT, service provider/systems integrator, Team Piccolo Global Enterprises employs over 20 professionals from different field, many of whom are well-trained and certified experts (Engineers/Academicians). We assist different organizations and individuals achieve their objectives through the state-of-the-art solutions we provide. We are passionate about supporting persons attaining their goal. We growing our business and driving operational excellence while making a positive impact on our community</p>
+        <div class="col-full">
+          <div class="col-extra">
+            <p>We aim to be our customers first choice in every area we serve by exceeding commitments, providing new technology solutions, leveraging our diverse brands, driving operational excellence, and committing to the highest standards of business practices all of which will drive Team Piccolo Global Enterprises long-term growth, value and success</p>
+            <p>Our team comprises of trained and certified experts in engineering and academic work, a number of whom have had experience of working on international: project design, management, implementation roles, and given academic talks. This team of engineers are assisted by support teams in the process of design, procurement, delivery and quality control to ensure that we provide value for every client, every time</p>
+          </div>
+          <div class="col-fill">
+            <img src="./images/cto.jpg" alt="Abba Jime Mustapha CTO Team Piccolo">
+            <p>Abba Jime Mustapha CTO Team Piccolo Global Enterprises</p>
+          </div>
         </div>
       </section>
     </main>
