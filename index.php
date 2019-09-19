bf0d7cf0612c96aa5e6c72eeca19925a71cf78d7
piccolo <kabiryusufbashir@gmail.com>
2019-09-18 22:14:17 -0700

    restructure the page, having the about us section first before our services section

--- index.php
+++ index.php
@@ -31,6 +31,28 @@
       </div>
     </header>
     <main>
+      <section id="about-team-piccolo">
+        <div class="col-full">
+          <div class="col-fill">
+            <img src="./images/ceo.jpg" alt="Kabir Yusuf Bashir CEO Team Piccolo">
+            <p>Kabir Yusuf Bashir CEO Team Piccolo Global Enterprises</p>
+          </div>
+          <div class="col-extra">
+            <p>Team Piccolo Global Enterprises is registered with Corporate Affairs Commission (C.A.C) under COMPANIES AND ALLIED MATTERS ACT 1990 on the 25th day of November, 2016 with a Business Number (B.N) 2462226</p>
+            <p>Over the years, Team Piccolo Global Enterprises has grown to a major ICT solution and Academic Writing provider in Nigeria and Africa, with major projects realized in different parts of the country and region</p>
+          </div>
+        </div>
+        <div class="col-full">
+          <div class="col-extra">
+            <p>We aim to be our customers first choice in every area we serve by exceeding commitments, providing new technology solutions, leveraging our diverse brands, driving operational excellence, and committing to the highest standards of business practices all of which will drive Team Piccolo Global Enterprises long-term growth, value and success</p>
+            <p>Our team comprises of trained and certified experts in engineering and academic work, a number of whom have had experience of working on international: project design, management, implementation roles, and given academic talks. This team of engineers are assisted by support teams in the process of design, procurement, delivery and quality control to ensure that we provide value for every client, every time</p>
+          </div>
+          <div class="col-fill">
+            <img src="./images/cto.jpg" alt="Abba Jime Mustapha CTO Team Piccolo">
+            <p>Abba Jime Mustapha CTO Team Piccolo Global Enterprises</p>
+          </div>
+        </div>
+      </section>
       <section id="our-services">
         <div class="col-quarter">
           <img src="./images/web-design.png" alt="Web Design Image">
@@ -81,28 +103,6 @@
               <li>Mastering and accessing referencing manager software such as EndNote, Mendale</li>
             </ul>
           </article>
-        </div>
-      </section>
-      <section id="about-team-piccolo">
-        <div class="col-full">
-          <div class="col-fill">
-            <img src="./images/ceo.jpg" alt="Kabir Yusuf Bashir CEO Team Piccolo">
-            <p>Kabir Yusuf Bashir CEO Team Piccolo Global Enterprises</p>
-          </div>
-          <div class="col-extra">
-            <p>Team Piccolo Global Enterprises is registered with Corporate Affairs Commission (C.A.C) under COMPANIES AND ALLIED MATTERS ACT 1990 on the 25th day of November, 2016 with a Business Number (B.N) 2462226</p>
-            <p>Over the years, Team Piccolo Global Enterprises has grown to a major ICT solution and Academic Writing provider in Nigeria and Africa, with major projects realized in different parts of the country and region</p>
-          </div>
-        </div>
-        <div class="col-full">
-          <div class="col-extra">
-            <p>We aim to be our customers first choice in every area we serve by exceeding commitments, providing new technology solutions, leveraging our diverse brands, driving operational excellence, and committing to the highest standards of business practices all of which will drive Team Piccolo Global Enterprises long-term growth, value and success</p>
-            <p>Our team comprises of trained and certified experts in engineering and academic work, a number of whom have had experience of working on international: project design, management, implementation roles, and given academic talks. This team of engineers are assisted by support teams in the process of design, procurement, delivery and quality control to ensure that we provide value for every client, every time</p>
-          </div>
-          <div class="col-fill">
-            <img src="./images/cto.jpg" alt="Abba Jime Mustapha CTO Team Piccolo">
-            <p>Abba Jime Mustapha CTO Team Piccolo Global Enterprises</p>
-          </div>
         </div>
       </section>
     </main>
