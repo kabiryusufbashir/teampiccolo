11b5a7224d691d0a55eaeef2bf16fa6a39b30512
piccolo <kabiryusufbashir@gmail.com>
2019-09-22 12:28:56 -0700

    Working on the images of CEO and CTO on the landing page

--- contact-us/index.php
+++ contact-us/index.php
@@ -8,13 +8,66 @@
       <div class="col-full bg-white">
         <h2><center>Contact Us</center></h2>
         <div class="col-full bg-white">
+          <?php
+						if(isset ($_POST['submit'])){
+
+							$fname = htmlentities($_POST['name']);
+							$from = htmlentities($_POST['email']);
+							$to = 'info@teampiccolo.com';
+							$phone = htmlentities($_POST['phone']);
+							$subject = htmlentities($_POST['subject']);
+							$message = htmlentities($_POST['message']);
+							$body = '<div style="font-size:17px;">'.$message .'</div> <br />Name: <b>'.$fname .'</b><br /> Phone: <b>'.$phone.'</b><br />Email: <b>'.$from.'</b><br /> Message was sent from <a href="www.teampiccolo.com"><b>teampiccolo.com</b></a>';
+
+							$encoding = "utf-8";
+
+							// Preferences for Subject field
+							$subject_preferences = array(
+								"input-charset" => $encoding,
+								"output-charset" => $encoding,
+								"line-length" => 76,
+								"line-break-chars" => "\r\n"
+							);
+
+							// Mail header
+							$header = "Content-type: text/html; charset=".$encoding." \r\n";
+							$header .= "From: ".$from." <".$from."> \r\n";
+							$header .= "MIME-Version: 1.0 \r\n";
+							$header .= "Content-Transfer-Encoding: 8bit \r\n";
+							$header .= "Date: ".date("r (T)")." \r\n";
+							$header .= iconv_mime_encode("Subject", $message, $subject_preferences);
+
+							// Send mail
+
+							if($fname && $from && $subject && $body){
+								mail($to, $subject, $body, $header);
+								echo "<center>Thank you for your mail, We will get back to you soon...</center>";
+								echo '
+									<script type="text/javascript">
+										function counter(time, url){
+											var interval = setInterval(function(){
+												time = time - 1;
+														if(time == 0){
+															clearInterval(interval);
+															window.location = url;
+														}
+											}, 1000)
+										}
+										counter(5, "index.php")
+									</script>
+								';
+							}else{
+                echo 'Please try again later. Thank You!!!';
+              }
+						}
+					?>
           <div class="col-fill">
-            <form action="index.html" method="post">
+            <form action="index.php" method="post">
               <input placeholder="Full Name" type="text" name="name" required="">
               <input placeholder="Subject" type="text" name="subject" required="">
               <input placeholder="Email" type="email" name="email" required="">
               <input placeholder="Phone" type="text" name="phone" required="">
-              <textarea name="message" required=""></textarea>
+              <textarea name="message" required="" placeholder="Message"></textarea>
               <input name="submit" type="submit" value="SEND">
             </form>
           </div>
@@ -25,7 +78,7 @@
               <img style="width: 50px; position:relative; top:15px;" src="../images/tel.png" alt="Telphone Icon">
             </span>
             <span style="width: 80%; position:relative; top:10px; left: 15px; float:left;">
-             08032338280<br> 08068593127<br> 08032856425
+             08032338280<br> 08068593127<br> info@teampiccolo.com
             </span>
           </div>
         </div>
