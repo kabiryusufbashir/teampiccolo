edca76281a6ebea5c664db8a3f2dde08e2abfcbd
piccolo <kabiryusufbashir@gmail.com>
2019-09-22 11:04:45 -0700

    Changed my mind regarding using bootstrap, I will be using vanilla CSS

--- contact-us/index.php
+++ contact-us/index.php
@@ -8,49 +8,18 @@
       <div class="col-full bg-white">
         <h2><center>Contact Us</center></h2>
         <div class="col-full bg-white">
-          <div class="col-sm-6">
-            <form action="./index.php" method="post">
-  						<div class="form-group">
-  							<label for="name">NAME*</label>
-  							<div class="input-group">
-  								<span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-  								<input class="form-control" type="text" name="name" required="">
-  							</div>
-  						</div>
-  						<div class="form-group">
-  							<label for="subject">SUBJECT*</label>
-  							<div class="input-group">
-  								<span class="input-group-addon"><i class="glyphicon glyphicon-record"></i></span>
-  								<input class="form-control" type="text" name="subject" required="">
-  							</div>
-  						</div>
-  						<div class="form-group">
-  							<label for="email">EMAIL*</label>
-  							<div class="input-group">
-  								<span class="input-group-addon"><i class="glyphicon glyphicon-envelope"></i></span>
-  								<input class="form-control" type="email" name="email" required="">
-  							</div>
-  						</div>
-  						<div class="form-group">
-  							<label for="phone">PHONE*</label>
-  							<div class="input-group">
-  								<span class="input-group-addon"><i class="glyphicon glyphicon-phone"></i></span>
-  								<input class="form-control" type="text" name="phone" required="">
-  							</div>
-  						</div>
-  						<div class="form-group">
-  							<label for="message">MESSAGE*</label>
-  							<div class="input-group">
-  								<span class="input-group-addon"><i class="glyphicon glyphicon-briefcase"></i></span>
-  								<textarea class="form-control" name="message" required=""></textarea>
-  							</div>
-  						</div>
-  						<div class="form-group">
-  							<input style="width:15%;" class="form-control btn btn-success" name="submit" type="submit" value="SEND">
-  						</div>
-  					</form>
+          <div class="col-fill">
+            <form action="index.html" method="post">
+              <input type="text" name="name" required="">
+              <input type="text" name="subject" required="">
+              <input type="email" name="email" required="">
+              <input type="text" name="phone" required="">
+              <textarea name="message" required=""></textarea>
+              <input style="width:15%;" name="submit" type="submit" value="SEND">
+
+            </form>
           </div>
-          <div class="col-sm-6"></div>
+          <div class="col-extra bg-white"></div>
         </div>
       </div>
     </main>
