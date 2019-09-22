86b10abdacd225d76a0b6e4ed668213178292b12
piccolo <kabiryusufbashir@gmail.com>
2019-09-21 21:04:21 -0700

    started writing the contents on the or team page

--- our-team/index.php
+++ our-team/index.php
@@ -6,16 +6,20 @@
     <?php headerSection('../'); ?>
     <main>
       <div class="col-full">
+        <h2><center>Meet Team Piccolo</center></h2>
         <div class="col-one-third">
-          <p>Kabir Yusuf Bashir CEO Team Piccolo Global Enterprises</p>
-          <p>Kabir Yusuf Bashir CEO Team Piccolo Global Enterprises</p>
+          <img src="../images/yusuf.png" alt="Kabir Yusuf Bashir Picture">
+          <p>Kabir Yusuf Bashir</p>
+          <p>
+            
+          </p>
         </div>
         <div class="col-one-third">
-          <p>Team Piccolo Global Enterprises is registered with Corporate Affairs Commission (C.A.C) under COMPANIES AND ALLIED MATTERS ACT 1990 on the 25th day of November, 2016 with a Business Number (B.N) 2462226</p>
+          <img src="../images/abba.png" alt="Abba Jime Mustapha Photo">
           <p>Over the years, Team Piccolo Global Enterprises has grown to a major ICT solution and Academic Writing provider in Nigeria and Africa, with major projects realized in different parts of the country and region</p>
         </div>
         <div class="col-one-third">
-          <p>Team Piccolo Global Enterprises is registered with Corporate Affairs Commission (C.A.C) under COMPANIES AND ALLIED MATTERS ACT 1990 on the 25th day of November, 2016 with a Business Number (B.N) 2462226</p>
+          <img src="../images/umar.png" alt="Umar Grema Photo">
           <p>Over the years, Team Piccolo Global Enterprises has grown to a major ICT solution and Academic Writing provider in Nigeria and Africa, with major projects realized in different parts of the country and region</p>
         </div>
       </div>
