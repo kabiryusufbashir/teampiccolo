bdc059141e6bbebb12b83383688ecfd1fa73f046
piccolo <kabiryusufbashir@gmail.com>
2019-09-21 21:28:44 -0700

    added jime's profile

--- our-team/index.php
+++ our-team/index.php
@@ -9,18 +9,48 @@
         <h2><center>Meet Team Piccolo</center></h2>
         <div class="col-one-third">
           <img src="../images/yusuf.png" alt="Kabir Yusuf Bashir Picture">
-          <p>Kabir Yusuf Bashir</p>
           <p>
-            
+            <h3>Kabir Yusuf Bashir</h3>
+            <i>Web Developer</i>
+          </p>
+          <p>
+            Kabir Yusuf Bashir from the family of Bashir Ibrahim Dan-Iya Kano, born on the 27th of July 1993 in Minjibir Local Government Kano State, Nigeria. I attended Wonderful Nursery and Primary School Garki, Abuja. I then moved to Community Nursery and Primary School Asokoro, Abuja. After completing my Primary School education, I then moved to Command Day Secondary School Maitama, where I completed my Secondary School education. Towards ending the year of 2012, I moved to Accra Ghana for my Bachelor Degree program where I studied Information Technology (I.T).
+          </p>
+          <p>
+            Kabir Yusuf Bashir specialized in Programming, Web programming to be precise. I have developed a lot of application like Hospital Management System, Examination Management System, Election Management System, Car Rental System, Newspaper Management System, Social Network (Membership Site), Memorization Tester (Quran) and currently working on School Management System.
           </p>
         </div>
         <div class="col-one-third">
           <img src="../images/abba.png" alt="Abba Jime Mustapha Photo">
-          <p>Over the years, Team Piccolo Global Enterprises has grown to a major ICT solution and Academic Writing provider in Nigeria and Africa, with major projects realized in different parts of the country and region</p>
+          <p>
+            <h3>Abba Jime Mustapha</h3>
+            <i>Network Engineer</i>
+          </p>
+          <p>
+            An Information Technologist with extensive experience in the field of Information Technology with an abundance of transferable skills.
+          </p>
+          <p>
+            Abba Jime Mustapha possess self-discipline and the ability to work with the minimum of supervision highly professional, ethical and discreet; demonstrates strong analytical and problem-solving competencies with the ability to make well thought out decisions;
+          </p>
+          <p>
+            Detail-oriented, efficient and organized professional with experience in ICT sectors; innovative in the design and execution of projects; effective in multi-tasking; excellent written and verbal communication skills.
+          </p>
         </div>
         <div class="col-one-third">
           <img src="../images/umar.png" alt="Umar Grema Photo">
-          <p>Over the years, Team Piccolo Global Enterprises has grown to a major ICT solution and Academic Writing provider in Nigeria and Africa, with major projects realized in different parts of the country and region</p>
+          <p>
+            <h3>Abba Jime Mustapha</h3>
+            <i>Network Engineer</i>
+          </p>
+          <p>
+            An Information Technologist with extensive experience in the field of Information Technology with an abundance of transferable skills.
+          </p>
+          <p>
+            Abba Jime Mustapha possess self-discipline and the ability to work with the minimum of supervision highly professional, ethical and discreet; demonstrates strong analytical and problem-solving competencies with the ability to make well thought out decisions;
+          </p>
+          <p>
+            Detail-oriented, efficient and organized professional with experience in ICT sectors; innovative in the design and execution of projects; effective in multi-tasking; excellent written and verbal communication skills.
+          </p>
         </div>
       </div>
     </main>
