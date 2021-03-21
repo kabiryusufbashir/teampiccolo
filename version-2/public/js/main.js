05fe0ff9b414420e7545a245ea0a41f2bd254327
Piccolo <kabiryusufbashir@gmail.com>
2021-03-21 21:17:27 +0100

    Mobile navigations

--- version-2/public/js/main.js
+++ version-2/public/js/main.js
@@ -1,10 +1,11 @@
 let menu = document.querySelector('#menu');
 let nav = document.querySelector('#nav');
+let close = document.querySelector('#close');
 
 menu.addEventListener('click', ()=>{
-    if(nav.classList.contains('hidden')){
-        nav.classList.remove('hidden');
-    }else{
-        nav.classList.add('hidden');
-    }
+    nav.classList.remove('hidden');
+});
+
+close.addEventListener('click', ()=>{
+    nav.classList.add('hidden');
 });
--- version-2/resources/views/layouts/template.blade.php
+++ version-2/resources/views/layouts/template.blade.php
@@ -7,14 +7,15 @@
         <title>@yield('page-title')</title>
         <link href="{{ asset('images/favicon.ico') }}" rel="icon">
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <link href="{{ asset('css/main.css') }}" rel="stylesheet">
     </head>
     <body>
         <!-- Navigation Bar -->
-        <div class="flex justify-between lg:grid grid-cols-5 gap-3 bg-white py-4 shadow lg:px-8 px-4 items-center">
+        <div class="flex justify-between lg:grid grid-cols-5 gap-3 bg-white py-4 shadow lg:px-8 px-4 items-center fixed w-full">
             <div class="lg:col-span-1">
                 <img class="w-12" src="{{ asset('images/logo.png') }}" alt="Team Piccolo Logo">
             </div>
-            <div id="nav" class="lg:col-span-3 hidden lg:block">
+            <div class="lg:col-span-3 hidden lg:block">
                 <nav class="lg:flex justify-between list-none">
                     <li><a href="#">Home</a></li>
                     <li><a href="#">Courses</a></li>
@@ -31,6 +32,23 @@
             <div id="menu" class="lg:hidden cursor-pointer">
                 <svg class="w-8 h-8" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
             </div>
+        </div>
+        <div id="nav" class="hidden h-screen relative bg-white pt-6">
+            <div class="float-right mr-4">
+                <svg id="close" class="w-10 h-10 cursor-pointer" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+            </div>
+            <nav class="list-none p-6 text-2xl">
+                <li class="p-6"><a href="#">Home</a></li>
+                <li class="p-6"><a href="#">Courses</a></li>
+                <li class="p-6"><a href="#">About Us</a></li>
+                <li class="p-6"><a href="#">Publications</a></li>
+                <li class="p-6"><a href="#">Blog</a></li>
+                <li class="p-6"><a href="#">Contact Us</a></li>
+            </nav>
+        </div>
+        <!-- header  -->
+        <div>
+            <img class="object-cover w-full h-screen" src="{{ asset('images/bg_2.jpg') }}" alt="Team Piccolo Header">
         </div>
         @yield('body-content')
         <script src="{{ asset('js/main.js') }}"></script>
