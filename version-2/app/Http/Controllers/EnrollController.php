a7b2de2d1fadbb71d3b972c17ecc713306e6f3be
Piccolo <kabiryusufbashir@gmail.com>
2021-04-05 12:25:18 +0100

    Works perfect

--- version-2/app/Http/Controllers/EnrollController.php
+++ version-2/app/Http/Controllers/EnrollController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Twilio\Rest\Client;
+
+use App\Models\User;
 
 class EnrollController extends Controller
 {
