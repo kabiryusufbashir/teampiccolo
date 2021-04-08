ec7f344736f0188c49942948d7b84f656c29af4f
Piccolo <kabiryusufbashir@gmail.com>
2021-04-08 17:49:02 +0100

    Added new features

--- version-2/resources/views/dashboard/index.blade.php
+++ version-2/resources/views/dashboard/index.blade.php
@@ -12,12 +12,12 @@
 </head>
 <body>
     <!-- Nav  -->
-    <div class="bg-blue-600">
+    <div class="bg-gray-600">
         <div class="md:flex md:justify-between md:grid md:grid-cols-4 py-3">
             <!-- system details -->
             <div class="mx-3 flex items-center justify-between col-span-1">
                 <img class="w-10" src="{{ $system_settings->photo ?? asset('images/logo.png') }}" alt="Logo">
-                <h2 class="text-white ml-2 text-lg">Team Piccolo</h2>
+                <h2 class="text-white ml-2 text-2xl">Team Piccolo</h2>
                 <svg id="menuCaret" class="w-8 h-8 text-white cursor-pointer" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
             </div>
             <!-- user details -->
@@ -30,13 +30,15 @@
                 <!-- Messages  -->
                 <div class="flex relative">
                     <svg class="w-6 h-6 text-white mx-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10c0 3.866-3.582 7-8 7a8.841 8.841 0 01-4.083-.98L2 17l1.338-3.123C2.493 12.767 2 11.434 2 10c0-3.866 3.582-7 8-7s8 3.134 8 7zM7 9H5v2h2V9zm8 0h-2v2h2V9zM9 9h2v2H9V9z" clip-rule="evenodd"></path></svg>
-                    <span class="bg-red-500 rounded-full px-1 absolute -top-3 left-8 w-auto text-center text-white">1008</span>
+                    <span class="bg-red-500 rounded-full px-1 absolute -top-3 left-8 w-auto text-center text-white">108</span> &nbsp;&nbsp;
                 </div>
                 <!-- User Account  -->
                 <div class="flex text-white items-center relative inline-block">
                     <span class="flex cursor-pointer" id="user_caret">
-                        <svg class="w-6 h-6 text-white ml-5 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z" clip-rule="evenodd"></path></svg>
-                        <b>{{auth()->user()->username}}</b>
+                        <!-- <svg class="w-6 h-6 text-white ml-5 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z" clip-rule="evenodd"></path></svg> -->
+                        <b class="relative top-1 font-normal">{{auth()->user()->name}}</b>
+                        &nbsp;
+                        <img class="w-10 h-10" src="{{ asset('images/yusuf.png') }}" alt="Logo">
                         <svg id="caret" class="w-5 h-5 my-auto" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>    
                     </span>
                     <div id="user_caret_menu" class="hidden absolute top-5 right-0 mt-2 w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
@@ -69,7 +71,7 @@
     <!-- #menu  -->
     <div class="md:grid md:grid-cols-5">
         <!-- Nav link  -->
-        <div id="#menu" class="hidden md:block px-3 bg-white col-span-1">
+        <div id="menu" class="hidden md:block px-3 bg-white col-span-1 shadow-lg">
             <h2 class="text-2xl py-3 border-b font-medium">Menu</h2>
             <ul>
                 <li class="py-3 flex border-b cursor-pointer">
@@ -94,21 +96,6 @@
                     </li>
                 </div>
 
-                <!-- Students -->
-                <li id="clientCaret" class="py-3 flex border-b cursor-pointer">
-                    <img class="w-7 mr-4" src="{{ asset('images/students_icon.png') }}" alt="Students">
-                    <a href="#">Students</a>
-                    <svg id="clientPointer" class="users-caret" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 15.707a1 1 0 010-1.414L14.586 10l-4.293-4.293a1 1 0 111.414-1.414l5 5a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0z" clip-rule="evenodd"></path><path fill-rule="evenodd" d="M4.293 15.707a1 1 0 010-1.414L8.586 10 4.293 5.707a1 1 0 011.414-1.414l5 5a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
-                </li>
-                <div id="clientMenu" class="users-caret-menu hidden">
-                    <li class="py-3 flex border-b">
-                        <a href="#">Add Students</a>
-                    </li>
-                    <li class="py-3 flex">
-                        <a href="#">All Students</a>
-                    </li>
-                </div>
-
                 <!-- Staff -->
                 <li id="clientCaret" class="py-3 flex border-b cursor-pointer">
                     <img class="w-7 mr-4" src="{{ asset('images/staff_icon.png') }}" alt="Staff">
@@ -121,6 +108,36 @@
                     </li>
                     <li class="py-3 flex">
                         <a href="#">All Staff</a>
+                    </li>
+                </div>
+
+                <!-- Financial Records -->
+                <li id="clientCaret" class="py-3 flex border-b cursor-pointer">
+                    <img class="w-7 mr-4" src="{{ asset('images/sales_icon.png') }}" alt="Staff">
+                    <a href="#">Financial Records</a>
+                    <svg id="clientPointer" class="users-caret" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 15.707a1 1 0 010-1.414L14.586 10l-4.293-4.293a1 1 0 111.414-1.414l5 5a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0z" clip-rule="evenodd"></path><path fill-rule="evenodd" d="M4.293 15.707a1 1 0 010-1.414L8.586 10 4.293 5.707a1 1 0 011.414-1.414l5 5a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
+                </li>
+                <div id="clientMenu" class="users-caret-menu hidden">
+                    <li class="py-3 flex border-b">
+                        <a href="#">Add Record</a>
+                    </li>
+                    <li class="py-3 flex">
+                        <a href="#">All Record</a>
+                    </li>
+                </div>
+
+                <!-- Students -->
+                <li id="clientCaret" class="py-3 flex border-b cursor-pointer">
+                    <img class="w-7 mr-4" src="{{ asset('images/students_icon.png') }}" alt="Students">
+                    <a href="#">Students</a>
+                    <svg id="clientPointer" class="users-caret" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 15.707a1 1 0 010-1.414L14.586 10l-4.293-4.293a1 1 0 111.414-1.414l5 5a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0z" clip-rule="evenodd"></path><path fill-rule="evenodd" d="M4.293 15.707a1 1 0 010-1.414L8.586 10 4.293 5.707a1 1 0 011.414-1.414l5 5a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
+                </li>
+                <div id="clientMenu" class="users-caret-menu hidden">
+                    <li class="py-3 flex border-b">
+                        <a href="#">Add Students</a>
+                    </li>
+                    <li class="py-3 flex">
+                        <a href="#">All Students</a>
                     </li>
                 </div>
 
