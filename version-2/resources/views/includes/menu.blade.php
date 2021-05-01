68d81b275c005f16bb2c78e24e11e46462dcb5c0
Piccolo <kabiryusufbashir@gmail.com>
2021-05-01 15:05:53 +0100

    Restructure the menu items

--- version-2/resources/views/includes/menu.blade.php
+++ version-2/resources/views/includes/menu.blade.php
@@ -60,6 +60,36 @@
             </li>
         </div>
 
+        <!-- Staff -->
+        <li id="staffCaret" class="py-3 flex border-b cursor-pointer">
+            <img class="w-7 mr-4" src="{{ asset('images/staff_icon.png') }}" alt="Staff">
+            <a href="#">Staff</a>
+            <svg id="staffPointer" class="users-caret" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 15.707a1 1 0 010-1.414L14.586 10l-4.293-4.293a1 1 0 111.414-1.414l5 5a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0z" clip-rule="evenodd"></path><path fill-rule="evenodd" d="M4.293 15.707a1 1 0 010-1.414L8.586 10 4.293 5.707a1 1 0 011.414-1.414l5 5a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
+        </li>
+        <div id="staffMenu" class="users-caret-menu hidden">
+            <li class="py-3 flex border-b">
+                <a href="#">Add Staff</a>
+            </li>
+            <li class="py-3 flex">
+                <a href="#">All Staff</a>
+            </li>
+        </div>
+
+        <!-- Students -->
+        <li id="clientCaret" class="py-3 flex border-b cursor-pointer">
+            <img class="w-7 mr-4" src="{{ asset('images/students_icon.png') }}" alt="Students">
+            <a href="#">Students</a>
+            <svg id="clientPointer" class="users-caret" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 15.707a1 1 0 010-1.414L14.586 10l-4.293-4.293a1 1 0 111.414-1.414l5 5a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0z" clip-rule="evenodd"></path><path fill-rule="evenodd" d="M4.293 15.707a1 1 0 010-1.414L8.586 10 4.293 5.707a1 1 0 011.414-1.414l5 5a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
+        </li>
+        <div id="clientMenu" class="users-caret-menu hidden">
+            <li class="py-3 flex border-b">
+                <a href="#">Add Students</a>
+            </li>
+            <li class="py-3 flex">
+                <a href="#">All Students</a>
+            </li>
+        </div>
+
         <!-- Clients -->
         <li id="clientCaret" class="py-3 flex border-t border-b cursor-pointer">
             <img class="w-7 mr-4" src="{{ asset('images/client_icon.png') }}" alt="client">
@@ -72,21 +102,6 @@
             </li>
             <li class="py-3 flex">
                 <a href="#">All Client</a>
-            </li>
-        </div>
-
-        <!-- Staff -->
-        <li id="clientCaret" class="py-3 flex border-b cursor-pointer">
-            <img class="w-7 mr-4" src="{{ asset('images/staff_icon.png') }}" alt="Staff">
-            <a href="#">Staff</a>
-            <svg id="clientPointer" class="users-caret" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 15.707a1 1 0 010-1.414L14.586 10l-4.293-4.293a1 1 0 111.414-1.414l5 5a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0z" clip-rule="evenodd"></path><path fill-rule="evenodd" d="M4.293 15.707a1 1 0 010-1.414L8.586 10 4.293 5.707a1 1 0 011.414-1.414l5 5a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
-        </li>
-        <div id="clientMenu" class="users-caret-menu hidden">
-            <li class="py-3 flex border-b">
-                <a href="#">Add Staff</a>
-            </li>
-            <li class="py-3 flex">
-                <a href="#">All Staff</a>
             </li>
         </div>
 
@@ -105,18 +120,18 @@
             </li>
         </div>
 
-        <!-- Students -->
+        <!-- NewsLetter -->
         <li id="clientCaret" class="py-3 flex border-b cursor-pointer">
-            <img class="w-7 mr-4" src="{{ asset('images/students_icon.png') }}" alt="Students">
-            <a href="#">Students</a>
+            <img class="w-7 mr-4" src="{{ asset('images/sales_icon.png') }}" alt="Staff">
+            <a href="#">NewsLetter</a>
             <svg id="clientPointer" class="users-caret" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 15.707a1 1 0 010-1.414L14.586 10l-4.293-4.293a1 1 0 111.414-1.414l5 5a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0z" clip-rule="evenodd"></path><path fill-rule="evenodd" d="M4.293 15.707a1 1 0 010-1.414L8.586 10 4.293 5.707a1 1 0 011.414-1.414l5 5a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
         </li>
         <div id="clientMenu" class="users-caret-menu hidden">
             <li class="py-3 flex border-b">
-                <a href="#">Add Students</a>
+                <a href="#">Add NewsLetter</a>
             </li>
             <li class="py-3 flex">
-                <a href="#">All Students</a>
+                <a href="#">All NewsLetters</a>
             </li>
         </div>
 
