15732f0a497ec865e07d5278350e5220f10ab53f
Piccolo <kabiryusufbashir@gmail.com>
2021-05-03 12:13:09 +0100

    Create show module on staff

--- version-2/resources/views/dashboard/staff/show.blade.php
+++ version-2/resources/views/dashboard/staff/show.blade.php
@@ -18,35 +18,46 @@
             <div class="">
                 <div class="bg-white mx-2 p-3">
                     <div class="flex justify-end">
-                        <a href="{{ route('student.all.index') }}">
+                        <a href="{{ route('staff.index') }}">
                             <button class="create-btn">
                                 <svg class="w-7 h-7" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clip-rule="evenodd"></path></svg> 
-                                All student
+                                All Staff
                             </button>
                         </a>
                     </div>
                     <div>
-                        @if($student->count())
+                        @if($staff->count())
                             <div class="shadow-lg p-4 lg:w-1/3 lg:mx-auto bg-green-600 text-white">
                                 <div class="my-6 mx-3 text-3xl font-medium">
-                                    <img class="w-32 h-32 md:w-48 md:h-48 mx-auto" src=" {{ $student->photo }}" alt=" {{ $student->name }} logo">
+                                    <img class="w-32 h-32 md:w-48 md:h-48 mx-auto" src=" {{ $staff->photo }}" alt=" {{ $staff->name }} logo">
                                 </div>
                                 <div class="my-2 p-2 mx-3 border-b  border-r">
-                                    Name: {{ $student->name }}
+                                    Name: {{ $staff->name }}
                                 </div>
                                 <div class="my-2 p-2 mx-3">
-                                    Email: {{ $student->email }}
+                                    Rank: {{ $staff->title }}
                                 </div>
                                 <div class="my-2 p-2 mx-3 border-t">
-                                    Phone: {{ $student->phone_number }}
+                                    Email: {{ $staff->email }}
+                                </div>
+                                <div class="my-2 p-2 mx-3 border-t">
+                                    Phone: {{ $staff->phone_number }}
+                                </div>
+                                <div class="my-2 p-2 mx-3 border-t">
+                                    Account Type: 
+                                        @if($staff->category == 1)
+                                            Super User
+                                        @else
+                                            Regular User
+                                        @endif
                                 </div>
                                 <div class="my-2 p-2 mx-3 border-l border-t">
-                                    Account Created: {{ $student->created_at->diffForHumans() }}
+                                    Account Created: {{ $staff->created_at->diffForHumans() }}
                                 </div>
                             </div>
                         @else
                             <div class="bg-white text-2xl text-center py-2">
-                                No Student Found
+                                No Staff Found
                             </div>
                         @endif
                     </div>
