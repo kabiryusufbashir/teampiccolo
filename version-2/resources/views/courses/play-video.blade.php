f1c544ccec4990397ad790ba7411f10388e3e60d
Piccolo <kabiryusufbashir@gmail.com>
2021-05-08 14:40:24 +0100

    Refactoring the edit profile module of courses

--- version-2/resources/views/courses/play-video.blade.php
+++ version-2/resources/views/courses/play-video.blade.php
@@ -1,20 +1,15 @@
-<html lang="en">
-<head>
-    <title>{{ $video->name }} | Team Piccolo Global Enterprises</title>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+@extends('layouts.template')
+
+@section('page-meta')
     <meta name="description" content="{{ $video->description }}">
     <meta name="keywords" content="HTML, CSS, JavaScript, Bootstrap, Enroll, Computer Essentials, Web Development, Mobile Applicaation, Entrepreneur,Information Technology, Kano, Training">
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@900&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
-    <link href="{{ asset('images/favicon.ico') }}" rel="icon">
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/main.css') }}" rel="stylesheet">
-</head>
-<body>
-    @include('layouts.courses-nav')
+@endsection
+
+@section('page-title')
+    {{ $video->name }} | Team Piccolo Global Enterprises
+@endsection
+
+@section('body-content')
     <!-- Play Video -->
     <div id="statsDiv" class="ml-2">
         <div class="text-2xl bg-white text-center border-b shadow py-2">
@@ -22,7 +17,7 @@
         </div>
         <div class="w-full lg:w-2/3 mx-auto shadow-lg py-4">
             <div class="bg-white mx-2 p-3">
-                <div class="flex justify-end">
+                <div class="back-btn">
                     <a href="{{ url()->previous() }}">
                         <button class="create-btn">
                         <svg class="w-7 h-7" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M8.445 14.832A1 1 0 0010 14v-2.798l5.445 3.63A1 1 0 0017 14V6a1 1 0 00-1.555-.832L10 8.798V6a1 1 0 00-1.555-.832l-6 4a1 1 0 000 1.664l6 4z"></path></svg>
@@ -54,6 +49,4 @@
             </div>
         </div>
     </div>
-    <script src="{{ asset('js/courses.js') }}"></script>
-</body>
-</html>
+@endsection
--- version-2/resources/views/courses/show-course.blade.php
+++ version-2/resources/views/courses/show-course.blade.php
@@ -17,7 +17,7 @@
         </div>
         <div class="w-full lg:w-4/5 mx-auto shadow-lg py-4">
             <div class="bg-white mx-2 p-3">
-                <div class="flex justify-end pt-14">
+                <div class="back-btn">
                     <a href="{{ url()->previous() }}">
                         <button class="create-btn">
                         <svg class="w-7 h-7" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M8.445 14.832A1 1 0 0010 14v-2.798l5.445 3.63A1 1 0 0017 14V6a1 1 0 00-1.555-.832L10 8.798V6a1 1 0 00-1.555-.832l-6 4a1 1 0 000 1.664l6 4z"></path></svg>
