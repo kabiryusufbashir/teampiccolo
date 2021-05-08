7c6b397a4b21c8a8b3fa7e6064f60bf6a844ec47
Piccolo <kabiryusufbashir@gmail.com>
2021-05-08 14:20:23 +0100

    Refactoring the index module of courses

--- version-2/resources/views/courses/index.blade.php
+++ version-2/resources/views/courses/index.blade.php
@@ -10,81 +10,33 @@
 @endsection
 
 @section('body-content')
-<div class="mx-3 my-6">
-        <div class="text-2xl bg-white text-center border-b shadow py-2">
-            @include('layouts.messages')
+    @if($courses->count())
+        <div class="page-title">
+            Team Piccolo Course 
         </div>
-        <div class="text-3xl mb-8">
-            <span class="border-b-4 border-green-600">Courses</span>
-        </div>
-        @if($courses->count() > 0)
-            <div class="my-6 mx-3">
-                {{ $courses->links() }}
-            </div>
-            <div class="md:grid md:grid-cols-4 md:gap-4">
-                <!-- Courses  -->
-                @foreach($courses as $course)
-                    <a href="{{ route('show.course', $course->id) }}">
-                        <div class="stats-card">
-                            <div>
-                                <img class="w-24 p-2" src="{{ $course->photo }}" alt="{{ $course->name }} Icon">
-                            </div>
-                            <div>
-                                <div class="text-right text-xl font-medium">Videos: {{ $course->video->count() }}</div>
-                                <div class="text-dark px-4 py-1 rounded-lg flex items-center">
-                                    <span>{{ $course->name }}</span>
-                                </div>    
-                            </div>
+        <div class="md:grid md:grid-cols-2 md:gap-4 md:gap-16 md:mx-24">
+            @foreach($courses as $course)
+                <a href="{{ route('show.course', $course->id) }}">
+                    <div class="stats-card hover:shadow-lg text-green-600 hover:bg-green-600 hover:text-white">
+                        <div>
+                            <img class="w-24 p-2" src="{{ $course->photo }}" alt="{{ $course->name }} Icon">
                         </div>
-                    </a>
-                @endforeach
-            </div>
-        @else
-            <div class="stats-card">
-                <div>
-                    <img class="stats-icon" src="{{ asset('images/logo.png') }}" alt="Logo Icon">
-                </div>
-                <div>
-                    <div class="bg-blue-400 text-white px-4 py-1 rounded-lg flex items-center">
-                        <span>Team Piccolo Course Module</span>
-                    </div>    
-                </div>
-            </div>
-        @endif
-    </div>
-    <div class="mt-6 border-t-2">
-        <div class="mx-3 my-4 text-3xl mb-8">
-            <span class="border-b-4 border-green-600">All Videos</span>
-        </div>
-        @if($videos->count())
-        <div class="md:grid md:grid-cols-5 md:gap-4 mx-2 my-6">
-            @foreach($videos as $video)
-                <div class="card border-2">
-                    <div>
-                        <img class="w-full p-2 mx-auto border-2" src=" {{ $video->photo }} " alt="{{ $video->name }} Image">    
+                        <div>
+                            <div class="text-right text-xl font-medium">Videos: {{ $course->video->count() }}</div>
+                            <div class="text-dark px-4 py-1 rounded-lg flex items-center">
+                                <span>{{ $course->name }}</span>
+                            </div>    
+                        </div>
                     </div>
-                    <div class="font-medium text-xl py-1 border-b">
-                        {{ $video->name }}
-                    </div>
-                    <div class="flex justify-center border-t pt-4 pb-2 items-center">
-                        <form action="{{ route('play.video', $video->id) }}">
-                            <button class="play-btn">
-                                <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM9.555 7.168A1 1 0 008 8v4a1 1 0 001.555.832l3-2a1 1 0 000-1.664l-3-2z" clip-rule="evenodd"></path></svg>
-                                &nbsp;&nbsp;
-                                <span> Play</span>
-                            </button>
-                        </form>
-                    </div>
-                </div>
+                </a>
             @endforeach
         </div>
-        <div class="my-6 mx-3">
-            {{ $videos->links() }}
+    @else
+        <div class="text-2xl lg:mx-32 px-4 my-6 relative top-10 my-24">
+            No Course found!
         </div>
-        @else
-            <div class="bg-white text-2xl text-center py-2">
-                No Video Found
-            </div>
-        @endif
+    @endif
+    <div class="my-6 mx-3">
+        {{ $courses->links() }}
     </div>
 @endsection
