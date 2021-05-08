e047ae356cadda4c00d2f46e7f3bfc2221e76451
Piccolo <kabiryusufbashir@gmail.com>
2021-05-08 18:19:07 +0100

    Done for now. Alhamdulilah

--- version-2/resources/views/about.blade.php
+++ version-2/resources/views/about.blade.php
@@ -10,14 +10,9 @@
 @endsection
 
 @section('body-content')
-<div class="lg:grid grid-cols-2 w-full flex justify-between leading-snug items-center">
-    <div class="hidden lg:block text-center bg-green-600 shadow-lg">
-        <img class="w-full object-cover mx-auto w-full" src="{{ asset('images/about.jpg') }}" alt="About Us">
-    </div>
-    <div class="my-auto mt-24 md:mt-16 w-full">
-        <div class="mx-10 lg:mx-16 text-3xl mb-8">
-            <span class="border-b-4 border-green-600">About Us</span>
-        </div>
+<div class="md:grid md:grid-cols-2 pt-24 md:pt-16 pb-6 md:gap-4 md:gap-16 md:mx-24">
+    <div>
+        <h1 class="border-b-2 border-green-600 text-green-600 mx-10 md:mx-16 text-4xl mb-2 py-2">About Us</h1>
         <p class="paragraph">
             Team Piccolo is a Software Solutions provider, I.C.T Training and Academic Writing provider in Nigeria and Africa, with major projects realized in different parts of the country.
         </p>
@@ -38,7 +33,20 @@
             Our team comprises of trained and certified experts in engineering and academic work, a number of whom have had experience of working on international: project design, management, implementation roles, and given academic talks. This team of engineers are assisted by support teams in the process of design, procurement, delivery and quality control to ensure that we provide value for every client, every time.
         </p><p class="paragraph">
             Our team comprises of trained and certified experts in engineering and academic work, a number of whom have had experience of working on international: project design, management, implementation roles, and given academic talks. This team of engineers are assisted by support teams in the process of design, procurement, delivery and quality control to ensure that we provide value for every client, every time.
-        </p> 
+        </p>
+    </div>
+    <div class="text-center mx-6 md:mx-24 text-green-600 md:my-auto">
+        @foreach($staffs as $staff)
+            <div class="card">
+                <div>
+                    <img class="p-2 mx-auto h-48" src=" {{ $staff->photo }} " alt="{{ $staff->name }} Image">    
+                </div>
+                <div class="font-medium text-xl py-1">
+                    {{ $staff->name }} <br>
+                    <span class="font-normal text-lg">{{  $staff->title }}</span>
+                </div>
+            </div>
+        @endforeach
     </div>
 </div>
 @endsection
