570fa8c2706c83cfa6ec036ff29117bf3aff17e7
Piccolo <kabiryusufbashir@gmail.com>
2021-05-08 18:25:30 +0100

    will work on error pages

---
+++ version-2/resources/views/errors/404.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.template')
+
+@section('page-meta')
+<meta name="description" content="Want to learn about Programming? Visit our blog to get the latest talks on programming.">
+<meta name="keywords" content="Writer, Blog, Entrepreneur, Information Technology, Kano, Training">
+@endsection
+
+@section('page-title')
+    Blog | Team Piccolo Global Enterprises
+@endsection
+
+@section('body-content')
+    <!-- error  -->
+    
+@endsection
