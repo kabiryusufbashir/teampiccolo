31396ba40754c6fc860d798422d74361bdc5aad5
Piccolo <kabiryusufbashir@gmail.com>
2021-05-08 15:10:41 +0100

    Added download column to ebook module to keep track of the number of downloads

--- version-2/resources/views/ebook.blade.php
+++ version-2/resources/views/ebook.blade.php
@@ -10,39 +10,31 @@
 @endsection
 
 @section('body-content')
-    <!-- ebook  -->
     @if($ebooks->count())
         <div class="page-title">
-            Team Piccolo Ebooks 
+            Team Piccolo ebook 
         </div>
-        <div class="lg:grid lg:grid-cols-3 gap-8 lg:mx-24">
-            <div class="col-span-2 my-auto">
-                @foreach($ebooks as $ebook)
-                <div class="w-full py-6 px-6 border-t-2">
-                    <h3 class="paragraph-title">
-                        <a class="hover:text-blue-600 hover:underline" target="_blank" href="{{ $ebook->path }}">
-                            {{ $ebook->name }}
-                        </a>
-                    </h3>
-                </div>
-                @endforeach
-            </div>
-            <div class="col-span-1 border-t-2 lg:border-t-0">
-                @foreach($staffs as $staff)
-                <div class="lg:ml-auto shadow-lg p-6 m-6">
-                    <img class="h-36 my-4 mx-auto" src="{{ $staff->photo }}" alt="{{ $staff->name }}">
-                    <h3 class="paragraph-title text-center">{{ $staff->name }}</h3>
-                    <h3 class="text-center text-green-500">{{ $staff->title }}</h3>
-                </div>
-                @endforeach            
-            </div>
+        <div class="md:grid md:grid-cols-2 md:gap-4 md:gap-16 md:mx-24">
+            @foreach($ebooks as $ebook)
+                <a href="{{ $ebook->path }}">
+                    <div class="stats-card hover:shadow-lg text-green-600 hover:bg-green-600 hover:text-white">
+                        <div>
+                            <div class="text-left px-4 text-xl font-medium">Download: {{ $ebook->download }}</div>
+                            <div class="text-dark px-4 py-1">
+                                <span>{{ $ebook->name }}</span>
+                            </div>    
+                        </div>
+                    </div>
+                </a>
+            @endforeach
         </div>
     @else
         <div class="text-2xl lg:mx-32 px-4 my-6 relative top-10 my-24">
-            No ebook Post found!
+            No Ebook found!
         </div>
     @endif
     <div class="my-6 mx-3">
         {{ $ebooks->links() }}
     </div>
+    
 @endsection
