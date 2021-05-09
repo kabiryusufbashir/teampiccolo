1060631d73d7bcf081f183a89696c435cc5e3cec
Piccolo <kabiryusufbashir@gmail.com>
2021-05-09 12:02:14 +0100

    Create error pages

--- version-2/resources/views/errors/500.blade.php
+++ version-2/resources/views/errors/500.blade.php
@@ -6,13 +6,13 @@
 @endsection
 
 @section('page-title')
-    Page Not Page | Team Piccolo Global Enterprises
+    Server Error | Team Piccolo Global Enterprises
 @endsection
 
 @section('body-content')
     <!-- error  -->
     <div class="text-center text-green-600 text-3xl pt-24 pb-6 md:p-10 md:m-4 md:mx-auto md:w-1/3">
-        <img class="w-48 mx-auto my-4" src="{{ asset('images/404.png') }}" alt="404 page">
-        Oops... Page not found 
+        <img class="w-48 mx-auto my-4" src="{{ asset('images/500.png') }}" alt="404 page">
+        Unable to handle your request now... Please try again later 
     </div>
 @endsection
